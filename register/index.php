4ecf2ef9dbe16eda9a271bcb0a9f28eb41e75b56
brown <gabriel.fraser223@gmail.com>
2016-02-06 21:42:09 +1100

    register: Started work

---
+++ register/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0">
+    <link rel="stylesheet" href="master.css" media="screen" title="no title" charset="utf-8">
+    <title>Register » Example.com</title>
+  </head>
+  <body>
+
+  </body>
+</html>
