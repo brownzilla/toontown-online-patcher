ded7ce00db64e0b562ca39552cbcbd5cb246e7b3
brown <gabriel.fraser223@gmail.com>
2016-02-06 23:00:25 +1100

    demo: Started using bootstrap

--- register/index.php
+++ register/index.php
@@ -1,12 +1,23 @@
+<?php include 'register.php'; ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, maximum-scale=1.0">
-    <link rel="stylesheet" href="master.css" media="screen" title="no title" charset="utf-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="master.css" charset="utf-8">
     <title>Register » Example.com</title>
   </head>
   <body>
+    <div class="container">
+      <div id="register-box">
+        <div class="panel panel-default">
+          <h1>Register</h1>
+          <form action="index.html" method="post">
 
+          </form>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
