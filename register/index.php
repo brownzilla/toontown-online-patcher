0c9ee3d453fce8304a0d15ac2f969e5ca13c87ff
brown <gabriel.fraser223@gmail.com>
2016-02-07 15:45:47 +1100

    web: Started work on registration page. x2

--- register/index.php
+++ register/index.php
@@ -5,18 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, maximum-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="master.css" charset="utf-8">
     <title>Register » Example.com</title>
   </head>
   <body>
     <div class="container">
-      <div id="register-box">
-        <div class="panel panel-default">
-          <h1>Register</h1>
-          <form action="index.html" method="post">
-
-          </form>
-        </div>
+      <div class="jumbotron text-center">
+        <h1><span class="fa fa-lock"></span> Toontown Online Patcher</h1>
+        <p>This is just a demo. You don't have to download the launcher if you're worried. <span class="fa fa-smile-o"></span></p>
       </div>
     </div>
   </body>
